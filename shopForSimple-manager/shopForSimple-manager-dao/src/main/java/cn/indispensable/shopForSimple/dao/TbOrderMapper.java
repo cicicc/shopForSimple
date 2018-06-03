bc6dcede2d701abf1be315652861b27562f01bd1
Chen Feng <afeng3990@163.com>
2018-06-03 22:44:09 +0800

    Initial commit

---
+++ shopForSimple-manager/shopForSimple-manager-dao/src/main/java/cn/indispensable/shopForSimple/dao/TbOrderMapper.java
@@ -0,0 +1,31 @@
+package cn.indispensable.shopForSimple.dao;
+
+import cn.indispensable.shopForSimple.pojo.TbOrder;
+import cn.indispensable.shopForSimple.pojo.TbOrderExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface TbOrderMapper {
+    int countByExample(TbOrderExample example);
+
+    int deleteByExample(TbOrderExample example);
+
+    int deleteByPrimaryKey(String orderId);
+
+    int insert(TbOrder record);
+
+    int insertSelective(TbOrder record);
+
+    List<TbOrder> selectByExample(TbOrderExample example);
+
+    TbOrder selectByPrimaryKey(String orderId);
+
+    int updateByExampleSelective(@Param("record") TbOrder record, @Param("example") TbOrderExample example);
+
+    int updateByExample(@Param("record") TbOrder record, @Param("example") TbOrderExample example);
+
+    int updateByPrimaryKeySelective(TbOrder record);
+
+    int updateByPrimaryKey(TbOrder record);
+}
---
+++ shopForSimple-manager/shopForSimple-manager-web/src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<%-- Created by IntelliJ IDEA. --%>
+<%@ page contentType="text/html;charset=UTF-8"  %>
+<html>
+<head>
+    <title>简单商城-为简单生活而生</title>
+</head>
+<body>
+<h1>简单商城 为简单生活而生</h1>
+</body>
+</html>
