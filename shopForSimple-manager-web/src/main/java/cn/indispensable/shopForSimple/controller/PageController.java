d1ac40ca13ba4b7670cec974e9619894ec882e71
Chen Feng <afeng3990@163.com>
2018-06-06 23:58:22 +0800

    #结构调整导致的bug修正

---
+++ shopForSimple-manager-web/src/main/java/cn/indispensable/shopForSimple/controller/PageController.java
@@ -0,0 +1,17 @@
+package cn.indispensable.shopForSimple.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    @RequestMapping("/")
+    public String showIndex() {
+        return "index";
+    }
+    @RequestMapping("/{page}")
+    public String showPage(@PathVariable String page) {
+        return "WEB-INF/jsp/"+page+".jsp";
+    }
+}
